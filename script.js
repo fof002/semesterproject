c2fcea515995b360125f74ffa76a4e5f6a3c9f7e
fof002 <80104002+fof002@users.noreply.github.com>
2022-05-11 18:03:06 +0200

    addded dropdown
    Added dropdown with JS

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById("mobile-navigation").addEventListener("click", openMenu);
 function openMenu () {
-    document.getElementById("dropdown").classList.toggle("active");
+    document.getElementById("dropdown").classList.toggle("active")
+}
+
+document.getElementById("mobile-navigation").addEventListener("click", changeIcon);
+function changeIcon () {
+    document.getElementById("mobile-navigation").classList.toggle("fa-x")
 }
